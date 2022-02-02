9da9bfbe1216c1dd4cc7ad69dd7ce3ef1c3d0a3e
RHns <96009866+RHns20@users.noreply.github.com>
2022-02-02 21:34:19 +0700

    commit

---
+++ plugins/main-donasi.js
@@ -0,0 +1,52 @@
+/**
+ * TOLONG JANGAN GANTI GAMBARNYA,NOMORKU DAN SAWERIAKU
+ * MENDING KALIAN TAMBAHIN NOMOR KALIAN
+*/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+let handler = async (m) => {
+let duit = `┌「 *Donasi - Pulsa* 」
+├ Axis [083832492541]
+├ Indosat [085856508824]
+└────
+
+┌「 *Donasi - Money* 」
+├ Dana [083832492541]
+└────`
+let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/bf14b1ce9249243b675e1.jpg' }}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: duit,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: 'Saweria',
+               url: 'https://saweria.co/botrhnsh'
+             }
+
+           },           
+               {
+             quickReplyButton: {
+               displayText: 'Owner',
+               id: '.owner',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)|bagiduit$/i
+
+module.exports = handler
