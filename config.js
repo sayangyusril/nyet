6d083b6e4937e1466753cfc92267d9c2c8b8de7f
RHns <96009866+RHns20@users.noreply.github.com>
2022-02-02 20:51:19 +0700

    commit

---
+++ config.js
@@ -0,0 +1,87 @@
+let fs = require('fs')
+
+global.owner = ['6283832492541'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  kotz: 'https://api.justkotz.tech',
+  xteam: 'https://api.xteam.xyz',
+  amel: 'https://melcanz.com',
+  lol: 'https://api.lolhuman.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'http://api.justkotz.tech': 'RHns',
+  'https://api.xteam.xyz': 'f592b4366be3922c',
+  'https://melcanz.com': 'RHns',
+  'https://api.lolhuman.xyz': 'rey2k21',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+}
+
+// Sticker WM
+global.packname = 'Made by'
+global.author = 'MarsBOT'
+global.dl = 'Download by : MarsBOT'
+global.benar = 'benar mendapatkan'
+global.salah = 'salah'
+global.dikit = 'hampir benar'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.wm = 'Made by : RHns'
+global.media = 'https://telegra.ph/file/aed723ee59556f948f9a0.jpg'
+
+global.wait = '*tunggu sedang diproses...*'
+global.eror = '*Server Error*'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      petFood: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
