b8249287409d8422616af603a209a028fe27a8a3
RHns <96009866+RHns20@users.noreply.github.com>
2022-02-02 23:20:05 +0700

    Update d-yta.js

--- plugins/d-yta.js
+++ plugins/d-yta.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) =
     let server = (args[1] || servers[0]).toLowerCase()
     let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
     let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-    m.reply(isLimit ? `Ukuran File: ${filesizeF}\nukuran File Diatas ${limit} MB, Download Sendiri: ${dl_link}` : wait)
+    m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran File Diatas ${limit} MB, Download Sendiri: ${dl_link}` : wait)
     if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
 *Titlel:* ${title}
 *Ukuran File:* ${filesizeF}
