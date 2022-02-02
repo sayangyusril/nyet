6d616cd1db4aae54fd406a7fbc7b87b3dd947f88
RHns <96009866+RHns20@users.noreply.github.com>
2022-02-02 23:27:42 +0700

    Create g-tagall.js

---
+++ plugins/g-tagall.js
@@ -0,0 +1,18 @@
+let handler = async(m, { conn, text, participants }) => {
+  let teks = `⋙ *Pesan dari Admin Group* ⋘
+\n *${text ? text : 'Nothing'}*\n\n`
+		      	for (let mem of participants) {
+		            teks += ` @${mem.id.split('@')[0]}\n`
+				}
+                teks += `\nbukan saya`
+                conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+}
+handler.help = ['tagall <pesan>']
+handler.tags = ['group']
+handler.command = /^(tagall)$/i
+
+handler.group = true
+handler.admin = true
+handler.owner = true
+
+module.exports = handler
