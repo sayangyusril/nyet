fa68f364c4e87b8a0f7468e26fb78d0f67558186
RHns <96009866+RHns20@users.noreply.github.com>
2022-02-02 23:18:33 +0700

    Update d-yta.js

--- plugins/d-yta.js
+++ plugins/d-yta.js
@@ -7,17 +7,16 @@ let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) =
     let server = (args[1] || servers[0]).toLowerCase()
     let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
     let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-    m.reply(isLimit ? `ukuran file: ${filesizeF}\nukuran file diatas ${limit} MB, download sendiri: ${dl_link}` : wait)
+    m.reply(isLimit ? `Ukuran File: ${filesizeF}\nukuran File Diatas ${limit} MB, Download Sendiri: ${dl_link}` : wait)
     if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
-*judul:* ${title}
-*ukuran file:* ${filesizeF}
+*Titlel:* ${title}
+*Ukuran File:* ${filesizeF}
 `.trim(), m, 0, { asDocument: chat.useDocument, mimetype: 'audio/mp4' })
 }
 handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
-handler.tags = ['download']
 handler.command = /^yt(a|mp3)$/i
 
-handler.limit = 1
+handler.limit = false
 
 module.exports = handler
 
