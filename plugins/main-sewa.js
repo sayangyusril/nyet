0690c0ab77870e5a31f773b709d30eb137bca378
RHns <96009866+RHns20@users.noreply.github.com>
2022-02-03 19:40:59 +0700

    Update main-sewa.js

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -1,16 +1,19 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERA*L, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
 let fs = require('fs')
 let handler = async (m) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-let sewa = `LIST SEWA BOT 
+let sewa = `*「 LIST SEWA BOT 」*
+
 10 Hari - 05,000
 30 Hari - 20,000
+
 Pulsa 
 Axis [083832492541] + 2k
 Indosat [085856508824] + 2k
+
 Non Pulsa
 Dana [083832492541]`
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
@@ -29,7 +32,7 @@ Dana [083832492541]`
            },
                {
              quickReplyButton: {
-               displayText: 'owner',
+               displayText: 'Owner',
                id: '.owner',
              }
 
